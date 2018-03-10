691bf0fda789e4894850eed4d5c7d13aaa77ac13
Soleg2008 <Soleg2008g@gmail.com>
2018-03-10 19:56:50 +0200

    zalil

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+
+$(function(){
+	 $('.slider').slick({
+        arrows: false,
+        autoplay: true
+      });
+	 $('.bg').slick({
+        arrows: true,
+        prevArrow:"<img class='slick-arrow slick-prev' src='images/prev.png'>",
+        nextArrow:"<img class='slick-arrow slick-next' src='images/next.png'>"
+      });
+});
+	
