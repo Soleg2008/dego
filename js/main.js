4391f4e9f17d67a4851ce46a74e56fb86af7fae2
Soleg2008 <Soleg2008g@gmail.com>
2018-03-11 22:38:38 +0200

    arrows

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@
 
 $(function(){
 	 $('.slider').slick({
-        arrows: false,
+        arrows: true,
         autoplay: true
       });
-	 $('.bg').slick({
-        arrows: true,
-        prevArrow:"<img class='slick-arrow slick-prev' src='images/prev.png'>",
-        nextArrow:"<img class='slick-arrow slick-next' src='images/next.png'>"
-      });
+	 // $('.bg').slick({
+  //       arrows: true,
+  //       prevArrow:"<img class='slick-arrow slick-prev' src='images/prev.png'>",
+  //       nextArrow:"<img class='slick-arrow slick-next' src='images/next.png'>"
+  //     });
 });
 	
